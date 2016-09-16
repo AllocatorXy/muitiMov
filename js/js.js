d5a658cc265cb7d2d62879643de896a76dff3b77
AllocatorXy <misaka20001@vip.qq.com>
2016-09-16 17:44:49 +0800

    init

---
+++ js/js.js
@@ -0,0 +1,41 @@
+window.onload = function () 
+{
+
+	var aDiv = document.getElementsByTagName('div');
+
+	function bufMov(obj,iTar) 
+	{
+		clearInterval(obj.timer);
+		obj.timer = setInterval(function () {
+			var speed = (iTar-obj.offsetWidth)/10;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+			if (obj.offsetWidth == iTar) 
+			{
+				clearInterval(obj.timer);
+			}
+			else
+			{
+				obj.style.width = obj.offsetWidth+speed+'px';
+			}
+		}, 15);
+	}
+	function msOver() 
+	{
+		bufMov(this,300+this.exLen);
+	}
+	function msOut() 
+	{
+		bufMov(this,70);
+	}
+
+	for (var i = 0; i < aDiv.length; i++) 
+	{
+		aDiv[i].timer = null; // 这里是关键，自定义属性obj.timer用来储存定时器返回值
+		aDiv[i].exLen = 50*i; // 追加长度，用于构建伸长后长度递增的效果
+
+		aDiv[i].onmouseover = msOver;
+		aDiv[i].onmouseout = msOut;
+	}
+
+};
